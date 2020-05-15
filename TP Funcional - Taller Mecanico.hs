cd68f580b210b632c723adad0d72d77ea19f0afe
Vueltero <vueltaagustin@gmail.com>
2020-05-15 20:46:42 -0300

    intente hacer el 1 pero me trabe con el enunciado

--- TP Funcional - Taller Mecanico.hs
+++ TP Funcional - Taller Mecanico.hs
@@ -1,4 +1,4 @@
-Todo auto tiene
+{-Todo auto tiene
 ●	la patente, que puede tener formato viejo "RVM363"
 o el nuevo "AB808RD"
 
@@ -11,7 +11,7 @@ motor, ej: 1500
 ●	la temperatura del agua luego de 5 minutos
 de encendido el auto: 90
 
-●	la fecha del último arreglo
+●	la fecha del último arreglo-}
 
 -------------------------------------------------------
 
@@ -33,3 +33,24 @@ data Auto = Auto
 } deriving (Show, Eq)
 
 -------------------------------------------------------
+
+--Punto 1 - "Costo de reparación de un auto"
+
+{-●	si la patente tiene 7 dígitos, es $ 12.500
+
+●	si no, si la patente está entre las
+	letras "DJ" y "NB"  , se aplica el calculoPatental
+	○	que es $ 3.000 * la longitud para
+		las patentes que terminen en 4
+	○	o $ 20.000 para el resto de las patentes
+
+●	de lo contrario, se le cobra $ 15000
+
+Los strings ("DJ", "NB") deben compararse con
+(<), (<=), (>), (>=)-}
+
+--costoReparacion :: Auto -> Int
+costoReparacion patente
+	| (length patente = 7) = 12500
+	| 
+	| otherwise = 15000
